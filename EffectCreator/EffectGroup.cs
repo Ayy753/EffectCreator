c5a6a237fbe93fdb734add6d41b81e9512929280
Thomas <thomas.sinka@dcmail.ca>
2021-10-04 21:28:08 -0400

    Added EffectGroup struct

---
+++ EffectCreator/EffectGroup.cs
@@ -0,0 +1,23 @@
+﻿namespace EffectCreator {
+    struct EffectGroup {
+        public string Name { get; }
+        public string Description { get; }
+        public float Radius { get; }
+        public TargetType Type { get; }
+        public string ParticleName { get; }
+        public SoundType SoundType { get; }
+        public float Cooldown { get; }
+        public IEffect[] Effects { get; }
+
+        public EffectGroup(string name, string description, float radius, TargetType type, string particleName, SoundType soundType, float cooldown, IEffect[] effects) {
+            Name = name;
+            Description = description;
+            Radius = radius;
+            Type = type;
+            ParticleName = particleName;
+            SoundType = soundType;
+            Cooldown = cooldown;
+            Effects = effects;
+        }
+    }
+}
--- EffectCreator/frmEffectGroup.cs
+++ EffectCreator/frmEffectGroup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,6 @@ namespace EffectCreator {
     public partial class frmEffectGroup : Form {
         public frmEffectGroup() {
             InitializeComponent();
-            EffectParser effectParser = new EffectParser();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
